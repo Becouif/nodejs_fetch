98b6d5c19ae20bb5b97183fd659792506b9d49eb
EnrichDD <enochonlinenow@gmail.com>
2021-11-05 14:42:42 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
 import React, {useState, useEffect} from 'react';
 
-const axios = require('axios');
 
 
 function App() {
   const [count, setCount] = useState(0);
   const [commit, setCommit] = useState([])
 
+  const commitUsers = async () => {
+    const response = await fetch('https://api.github.com/repos/nodejs/node/commits');
+    const finalData = await response.json();
+    finalData.slice(0,25);
+    setCommit(finalData)
+  }
+  
 
   useEffect(() => {
-    document.title = `You clicked ${count} times`;
-    axios({
-      method: 'get',
-      url: 'https://api.github.com/repos/nodejs/node/commits',
-      responseType: 'arraybuffer'
-    })
-      .then(function (response) {
-        let rawCommit = response.data
-        rawCommit = rawCommit.slice(0,25);
-        
-        rawCommit.map((each) =>{
-          console.log(each.commit)
-
-        })
-        // console.log(rawCommit[0].commit)
-        })
-      .catch((err) =>{
-        console.log(err)
-      });
-  });
-  
-  
+    commitUsers();
+  }, [])
 
 
   return (
     <div>
-    
+   { commit.map((eachCommit) => {
+      return (
+        <p>{eachCommit.commit.message}</p>
+      )
+    })}
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
